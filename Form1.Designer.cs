cee573d0531955daba1f8cc431de150e4333fc28
litov74dev <levt382@gmail.com>
2021-02-22 02:38:02 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace GuessMyNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberOutput = new System.Windows.Forms.Label();
+            this.ruler = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(101, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Угадашка";
+            // 
+            // numberOutput
+            // 
+            this.numberOutput.AutoSize = true;
+            this.numberOutput.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberOutput.Location = new System.Drawing.Point(203, 58);
+            this.numberOutput.Name = "numberOutput";
+            this.numberOutput.Size = new System.Drawing.Size(0, 52);
+            this.numberOutput.TabIndex = 2;
+            // 
+            // ruler
+            // 
+            this.ruler.AutoSize = true;
+            this.ruler.Location = new System.Drawing.Point(181, 144);
+            this.ruler.Name = "ruler";
+            this.ruler.Size = new System.Drawing.Size(0, 20);
+            this.ruler.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "до 100";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "до 10000";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(162, 258);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 32);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "до 1000";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(111, 330);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(207, 58);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Всё *ня, давай поновой";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 400);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ruler);
+            this.Controls.Add(this.numberOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Убей свое время";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label numberOutput;
+        private System.Windows.Forms.Label ruler;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using FizzWare.NBuilder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessMyNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            numberOutput.Text = "";
+        }
+        int number = 0;
+        int attempt = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            number = rand.Next(0, 100);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            number = rand.Next(0, 1000);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            number = rand.Next(0, 10000);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (textBox1.Text == number.ToString())
+                {
+                    numberOutput.ForeColor = Color.Green;
+                    numberOutput.Text = "Yes";
+                    ruler.Text = "";
+                }
+                if (textBox1.Text != number.ToString())
+                {
+                    if (int.Parse(textBox1.Text) > number)
+                    {
+                        ruler.Text = "Мое число меньше";
+                        
+                    }
+                    else
+                    {
+                        ruler.Text = "Мое число больше";
+                        
+                    }
+                    numberOutput.ForeColor = Color.Red;
+                    numberOutput.Text = "No";
+                    textBox1.Text = "";
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ruler.Text = "";
+            numberOutput.Text = "";
+            textBox1.Text = "";
+        }
+    }
+}
